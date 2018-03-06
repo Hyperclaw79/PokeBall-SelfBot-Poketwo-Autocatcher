ffc92c7deecdf9b707222ce80e1cb187ed236b7d
Harshith Thota <harshith.thota7@gmail.com>
2018-03-07 01:48:32 +0530

    Fixed wrongly tracked files.

--- secret.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def alter_config(path):
-    with open(path,'r') as f:
-        configs = json.load(f)
-    configs["token"] = "MzYyOTU1NDEwMjU4NTI2MjA5.DX8_xg.h-gkv1wzzizMkeZm8LZ6EnE2AQ4"
-    with open('config.json','w') as f:
-        f.write(json.dumps(configs,indent=3))
