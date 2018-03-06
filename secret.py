beb4e40e960c3af976a8dea9153aca535eb28f45
Harshith Thota <harshith.thota7@gmail.com>
2018-03-07 01:55:03 +0530

    Uploaded Missing Files

---
+++ secret.py
@@ -0,0 +1,9 @@
+import os
+import json
+
+def alter_config(path):
+    with open(path,'r') as f:
+        configs = json.load(f)
+    configs["token"] = os.environ["DISCORD_BOT_TOKEN"]
+    with open('config.json','w') as f:
+        f.write(json.dumps(configs,indent=3))
