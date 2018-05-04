2a06874ba78345c45b8c07c481a43021a08b7372
Harshith Thota <harshith.thota7@gmail.com>
2018-05-04 11:51:34 +0530

    Added encoding as utf-8
    Possible fix for https://github.com/Hyperclaw79/PokeBall-SelfBot/issues/13.

--- secret.py
+++ secret.py
@@ -2,7 +2,7 @@ import os
 import json
 
 def alter_config(path):
-    with open(path,'r') as f:
+    with open(path,'r', encoding='utf-8') as f:
         configs = json.load(f)
     configs["token"] = os.environ["DISCORD_BOT_TOKEN"]
     with open('config.json','w') as f:
